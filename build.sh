b563d8280127a00ad885cf678f04d74736be2900
Bouabid Moukhlis <bobmoukhlis@gmail.com>
2025-12-23 03:04:26 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "🔧 Aggiorno pip e setuptools..."
-pip install --upgrade pip setuptools wheel
-
-echo "📦 Installo i pacchetti richiesti..."
-pip install -r requirements.txt || exit 1
-
-echo "✅ Build completata!"
